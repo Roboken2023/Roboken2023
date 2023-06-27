60af01fe93c800005e3bba3974ebda3c3a43d064
Treyul <emmanuelryley55@gmail.com>
2023-06-27 13:39:02 +0300

    add file

---
+++ file_heirachy.py
@@ -0,0 +1,29 @@
+"""
+        **** Heirachy  of files ****
+        Motor_varibles.h --> Contains the pins that will be used for controlling the motors and line following
+                           parameters.
+            
+        movements.h --> Contain motor controlling functions and Ultrasonic readings
+
+        Variables.h  --> contains all function names and global variables initializations
+
+        functions.h  --> Conatins functions for task executions and positioning function
+
+        Dojo23.ino --> Main function runs the functions.
+
+        motor_variables.h
+                |
+                |
+            movements.h                  variables.h
+            |                                 |
+            |                                 |
+            |                                 |
+            -----------------------------------
+                            |
+                            |
+                        functions.h
+                            |
+                            |
+                        Dojo23.ino 
+
+"""
